cfb09758a09466c4c506218dafe7b9e59430cd1b
kupark <pouu69@gmail.com>
2016-03-28 16:15:24 +0900

    make

---
+++ application/core/CAFES_Model.php
@@ -0,0 +1,7 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class CAFES_Model extends CI_Model {
+        public function _construct() {
+                parent::_construct();
+        }
+}
---
+++ application/core/CAFES_REST_Controller.php
@@ -0,0 +1,8 @@
+<?php
+require APPPATH.'/libraries/REST_Controller.php';
+
+class CAFES_REST_Controller extends REST_Controller {
+        function __construct() {
+                parent::__construct();
+        }
+}
