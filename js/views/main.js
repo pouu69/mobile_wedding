4427d0aaff3261e4d4498ed4c747df3fa70827d2
kupark <pouu69@gmail.com>
2016-03-29 19:11:04 +0900

    stpe3

--- js/views/main.js
+++ js/views/main.js
@@ -3,11 +3,14 @@
  */
 function scrollEvt(){
     $(window).on("scroll", function() {
-        if($(window).scrollTop() > 690) {
+        if($(window).scrollTop() > 220) {
             $(".header-wrap").addClass("active");
+            $('.header .header-wrap .header-li-main span').addClass('active');
         } else {
             //remove the background property so it comes transparent again (defined in your css)
             $(".header-wrap").removeClass("active");
+            $('.header .header-wrap .header-li-main span').removeClass('active');
+
         }
     });
 }
